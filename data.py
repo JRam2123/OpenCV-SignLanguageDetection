e53e3989dada8b4d45edd4e05bdda4b9cff10a47
Jordan Ramirez <115486642+JRam2123@users.noreply.github.com>
2022-10-24 13:31:14 -0600

    Add files via upload

---
+++ data.py
@@ -0,0 +1,57 @@
+import cv2 as cv
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+import time
+cap = cv.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+offset = 20
+imgSize = 300
+
+folder = "images/C"
+counter = 0
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+
+        imgCrop = img[y-offset:y + h+offset, x-offset:x + w+offset]
+
+        imgCropShape = imgCrop.shape
+
+
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+
+        cv.imshow("ImageCrop", imgCrop)
+        cv.imshow("ImageWhite", imgWhite)
+
+
+    cv.imshow("Image", img)
+    key = cv.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+        print(counter)
+
+
---
+++ test.py
@@ -0,0 +1,65 @@
+import cv2 as cv
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+import time
+cap = cv.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
+offset = 20
+imgSize = 300
+
+folder = "images/C"
+counter = 0
+
+labels = ["A", "B", "C"]
+
+while True:
+    success, img = cap.read()
+    imgOutput = img.copy()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+
+        imgCrop = img[y-offset:y + h+offset, x-offset:x + w+offset]
+
+        imgCropShape = imgCrop.shape
+
+
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
+
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+        cv.putText(imgOutput, labels[index], (x, y-20), cv.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 2)
+        cv.rectangle(imgOutput, (x-offset,y-offset), (x + w+offset,y + h+offset), (255,0,255),4)
+
+        cv.imshow("ImageCrop", imgCrop)
+        cv.imshow("ImageWhite", imgWhite)
+
+
+    cv.imshow("Image", imgOutput)
+    cv.waitKey(1)
+
+
